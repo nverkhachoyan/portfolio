fe3f0c8ad3baba1c4eb06c663ff724e80a87d92c
Nver Khachoyan <khachoyannver@gmail.com>
2022-08-16 21:37:24 -0700

    added projects, links, increased font of greeting

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,7 @@
 import './Projects.css';
+import chromeExtImg from "../assets/images/projects/chromeextension.png"
+import teachyImg from "../assets/images/projects/teachy.png"
+import comingSoonImg from "../assets/images/projects/comingsoon.jpeg"
 
 function Projects() {
   return (
@@ -9,27 +12,30 @@ function Projects() {
         <div className='projects-container'>
         <div className="project">
                 <h3 className='project-title'>Teachy</h3>
-                <p className='project-body'>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Deleniti porro, commodi in cumque blanditiis ad aliquid ut 
-                    ratione doloribus, quidem ex quo totam dolor, praesentium cum 
-                    expedita odit iste! Excepturi.
+                <img className='project-image' src={teachyImg} alt="screenshot of my project, a chrome extension" />
+                <p className='project-body'>
+                    A very convenient way to manage classrooms. It will have features such as taking attendance,
+                    grading, creating random groups of students, and more. Work in progress.
                 </p>   
+                <a className='project-link' href="https://github.com/nverkhachoyan/teachy" target="_blank" rel="noopener noreferrer">View on Github</a>
             </div>
             <div className="project">
-                <h3 className='project-title'>Teachy</h3>
-                <p className='project-body'>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Deleniti porro, commodi in cumque blanditiis ad aliquid ut 
-                    ratione doloribus, quidem ex quo totam dolor, praesentium cum 
-                    expedita odit iste! Excepturi.
+                <h3 className='project-title'>Chrome Extension</h3>
+                <img className='project-image' src={chromeExtImg} alt="screenshot of my project, a chrome extension" />
+                <p className='project-body'>
+                    A clean and simple chrome extension with the date, time and a daily task list system. Weather information will be added soon.
                 </p>   
+                <a className='project-link' href="https://github.com/nverkhachoyan/chrome-extension" target="_blank" rel="noopener noreferrer">View on Github</a>
             </div>
             <div className="project">
-                <h3 className='project-title'>Teachy</h3>
+                <h3 className='project-title'>Coming Soon</h3>
+                <img className='project-image' src={comingSoonImg} alt="screenshot of my project, a chrome extension" />
                 <p className='project-body'>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
                     Deleniti porro, commodi in cumque blanditiis ad aliquid ut 
                     ratione doloribus, quidem ex quo totam dolor, praesentium cum 
                     expedita odit iste! Excepturi.
                 </p>   
+                <a className='project-link' href="#" target="_blank" rel="noopener noreferrer">View on Github</a>
             </div>
         </div>
     </section>
